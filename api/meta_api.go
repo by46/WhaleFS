bf2233680f10558f022a9c515c55b3c249bebe03
benjamin.c.ya <ycs_ctbu_2010@126.com>
2019-05-14 14:41:29 +0800

    feat: support gocb

--- api/meta_api.go
+++ api/meta_api.go
@@ -20,16 +20,27 @@ func NewMetaClient(connectionString string, password string) common.Meta {
 	if err != nil {
 		panic(errors.Wrapf(err, "initialize meta client failed: %s", connectionString))
 	}
-	result.Scheme = "http"
 	bucketName := strings.Trim(result.Path, "/")
+	user := result.User
+	result.Scheme = "http"
+	result.User = nil
 	result.Path = ""
 
-	cluster, err := gocb.Connect(result.Path)
+	cluster, err := gocb.Connect(result.String())
 	if err != nil {
 		panic(err)
 	}
+	if password, passwordSet := user.Password(); passwordSet {
+		err = cluster.Authenticate(gocb.PasswordAuthenticator{
+			Username: user.Username(),
+			Password: password,
+		})
+		if err != nil {
+			panic(err)
+		}
+	}
 
-	bucket, err := cluster.OpenBucket(bucketName, password)
+	bucket, err := cluster.OpenBucket(bucketName, "")
 	if err != nil {
 		panic(err)
 	}
