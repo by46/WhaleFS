9253538259ce5e99866599769e05809aed280bd1
mark.c.jiang <mark.c.jiang@yzw.cn>
2019-05-27 10:05:47 +0800

    Refactor ansible deploy script

--- scripts/ansible_deploy.sh
+++ scripts/ansible_deploy.sh
@@ -1,10 +1,12 @@
 #!/bin/bash -e
 
 REPO_DIR=$WORKSPACE
+TARGET="$target"
+ENV=${PARAM_HOST_ARRAY[0]}
 
 ansible all -i "localhost," -m git -a "repo=git@cmisgitlab01:framework/yzw-playbooks.git dest=./yzw-playbooks/ version=develop" --connection=local
 
 cd ./yzw-playbooks
 
-ansible-playbook playbooks/whalefs/qa/deploy_filer.yml -e app_version=develop -e need_checkout_source=false -e repo_dir=$REPO_DIR -e repo_name="" -t deploy
+ansible-playbook playbooks/whalefs/$ENV/deploy_$TARGET.yml -e app_version=develop -e need_checkout_source=false -e repo_dir=$REPO_DIR -e repo_name="" -t deploy
 
