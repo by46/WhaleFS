00f3b6486c86757645e2432058df29a6daa44ab6
mark.c.jiang <mark.c.jiang@yzw.cn>
2019-05-28 09:29:45 +0800

    Fix bug

--- scripts/ansible_deploy.sh
+++ scripts/ansible_deploy.sh
@@ -12,4 +12,4 @@ ansible all -i "localhost," -m git -a "repo=git@cmisgitlab01:framework/yzw-playb
 
 cd ./yzw-playbooks
 
-ansible-playbook playbooks/whalefs/deploy.yml -e app_version=develop -e $ENV -e target=$TARGET -e need_checkout_source=false -e repo_dir=$REPO_DIR -e repo_name="" -t deploy
+ansible-playbook playbooks/whalefs/deploy.yml -e app_version=develop -e env=$ENV -e target=$TARGET -e need_checkout_source=false -e repo_dir=$REPO_DIR -e repo_name="" -t deploy
