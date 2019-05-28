74c92aba9ea201f5df9d7c53171e133d5a97f7cf
mark.c.jiang <mark.c.jiang@yzw.cn>
2019-05-28 09:29:28 +0800

    Fix bug and refactor deploy script

--- scripts/ansible_deploy.sh
+++ scripts/ansible_deploy.sh
@@ -4,9 +4,12 @@ REPO_DIR=$WORKSPACE
 TARGET="$target"
 ENV=${PARAM_HOST_ARRAY[0]}
 
+# golang build environment
+export HOME=/root
+export PATH=/opt/go1.12/bin/:$PATH
+
 ansible all -i "localhost," -m git -a "repo=git@cmisgitlab01:framework/yzw-playbooks.git dest=./yzw-playbooks/ version=develop" --connection=local
 
 cd ./yzw-playbooks
 
-ansible-playbook playbooks/whalefs/$ENV/deploy_$TARGET.yml -e app_version=develop -e need_checkout_source=false -e repo_dir=$REPO_DIR -e repo_name="" -t deploy
-
+ansible-playbook playbooks/whalefs/deploy.yml -e app_version=develop -e $ENV -e target=$TARGET -e need_checkout_source=false -e repo_dir=$REPO_DIR -e repo_name="" -t deploy
