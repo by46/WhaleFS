32ed4e9b36e78938bdbd236fde3a8fb97b7824d9
benjamin.c.ya <ycs_ctbu_2010@126.com>
2019-07-01 09:20:54 +0800

    feat: fix bug

--- portal/src/components/layouts/portal.vue
+++ portal/src/components/layouts/portal.vue
@@ -1,44 +1,48 @@
 <template>
     <el-container style="height: 100vh; border: 1px solid #eee">
         <el-header class="header">
-            <div class="logo">
-                <img src="../../assets/logo.png" alt="whalefs">
-            </div>
-            <div class="right-header">
-                <div v-if="username != ''">
-                    <span class="user">当前用户: {{ username }}</span>
-                    <el-button style="padding: 0px"
-                               type="text"
-                               @click="onLogout">
-                        退出
-                    </el-button>
+            <el-menu
+                    style="padding-left: 200px;"
+                    class="el-menu-demo"
+                    @select="handleSelect"
+                    mode="horizontal"
+                    background-color="#545c64"
+                    text-color="#fff"
+                    active-text-color="#ffd04b">
+                <el-menu-item index="/portal">Home</el-menu-item>
+                <el-menu-item index="/portal/buckets">Buckets</el-menu-item>
+                <el-menu-item index="/portal/users">Users</el-menu-item>
+                <div class="right-header">
+                    <div v-if="username != ''">
+                        <span class="user">当前用户: {{ username }}</span>
+                        <el-button style="padding: 0px"
+                                   type="text"
+                                   @click="onLogout">
+                            退出
+                        </el-button>
+                    </div>
+                    <div v-else>
+                        <el-button style="padding: 0px"
+                                   type="text"
+                                   @click="onLogin">
+                            登录
+                        </el-button>
+                    </div>
                 </div>
-                <div v-else>
-                    <el-button style="padding: 0px"
-                               type="text"
-                               @click="onLogin">
-                        登录
-                    </el-button>
-                </div>
-            </div>
-        </el-header>
+            </el-menu>
 
+        </el-header>
         <el-container>
-            <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
-                <el-menu :router="true" class="totalUl" :default-active="$route.path">
-                    <el-menu-item index="/portal/dashboard"><i class="el-icon-pie-chart"></i>Dashboard</el-menu-item>
-                    <el-menu-item index="/portal/buckets"><i class="el-icon-delete"></i>Buckets</el-menu-item>
-                    <el-menu-item index="/portal/users"><i class="el-icon-user"></i>Users</el-menu-item>
-                </el-menu>
-            </el-aside>
             <el-main>
-                <section class="content">
-                    <transition
-                            name="page"
-                            mode="out-in">
-                        <router-view></router-view>
-                    </transition>
-                </section>
+                <div style="width:800px;margin: auto;">
+                    <section class="content">
+                        <transition
+                                name="page"
+                                mode="out-in">
+                            <router-view></router-view>
+                        </transition>
+                    </section>
+                </div>
             </el-main>
         </el-container>
     </el-container>
@@ -67,6 +71,9 @@
       },
       onLogin() {
         this.$router.push({path: '/login'})
+      },
+      handleSelect(key) {
+        this.$router.push({path: key})
       }
     }
   }
@@ -74,13 +81,8 @@
 
 <style scoped>
     .header {
-        color: rgba(255, 255, 255, 0.75);
         line-height: 60px;
-        background-color: #24292e;
-    }
-
-    .header div {
-        display: inline;
+        padding: 0;
     }
 
     .totalUl {
@@ -89,6 +91,7 @@
 
     .right-header {
         float: right;
+        padding-right: 30px;
     }
 
     .user {
